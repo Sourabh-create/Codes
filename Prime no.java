a50d30d7168dd65c3dfef58ab4dc48bac57a24fe
Sourabh Yadav <67201962+Sourabh-create@users.noreply.github.com>
2022-12-12 03:31:22 +0530

    Add files via upload

---
+++ Prime no.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n;
+	    int count =0;
+	    n=sc.nextInt();
+	    if(n<2)
+	    System.out.println(+n+ " is not a prime number");
+	    
+	    for(int i=1;i<=n;i++)
+	    {
+	        if(n%i==0)
+	        count +=1;
+	    }
+	    if (count>2)
+	     System.out.println(+n+ " is not a prime no");
+	     else 
+	     System.out.println(+n+" is a prime no.");
+	}
+}
