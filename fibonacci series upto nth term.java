ed570dd9550df9da49b0d82b55289857210ef529
Sourabh Yadav <67201962+Sourabh-create@users.noreply.github.com>
2022-12-16 15:05:55 +0530

    Update fibonacci series upto nth term.java

--- fibonacci series upto nth term.java
+++ fibonacci series upto nth term.java
@@ -3,16 +3,15 @@ public class Main {
    public static void main(String args[]){
       Scanner sc = new Scanner(System.in);
       int a=0,b=1;
-      int sum =0;
       int n;
       System.out.println("Enter nth term");
       n=sc.nextInt();
-      while(sum<=n)
-      {
+      System.out.print(a+" "+ b +" ");
+      for(int i=1;i<=n-2;i++){
+          int sum = a+b;
           System.out.print(sum + " ");
           a=b;
           b=sum;
-          sum=a+b;
       }
    }
 }
