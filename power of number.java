dd3823009b3e35fe823e276fa21646fbe8ddf472
Sourabh Yadav <67201962+Sourabh-create@users.noreply.github.com>
2022-12-19 18:32:10 +0530

    Update power of number.java

--- power of number.java
+++ power of number.java
@@ -13,3 +13,27 @@ public class Main {
       System.out.println("Answer = " + result);
    }
 }
+
+
+
+//using for loop(methord 2)
+
+import java.util.Scanner;
+public class Main {
+   public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      int base;
+      int power;
+      System.out.println("Enter Base");
+      base=sc.nextInt();
+      System.out.println("Enter expo");
+      power=sc.nextInt();
+      double result=1;
+      
+      for(int i=1;i<=power;i++)
+      result=result*base;
+      {
+          System.out.print("Answer= "+ result);
+      }
+   }
+}
