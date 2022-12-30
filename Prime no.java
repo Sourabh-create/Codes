3b66704849559a29d0bc1f08fc4eac8cb66c2a1e
Sourabh Yadav <67201962+Sourabh-create@users.noreply.github.com>
2022-12-30 17:11:27 +0530

    Update Prime no.java

--- Prime no.java
+++ Prime no.java
@@ -5,18 +5,20 @@ public class Main
 	    Scanner sc = new Scanner(System.in);
 	    int n;
 	    int count =0;
+	    System.out.println("Enter number:");
 	    n=sc.nextInt();
-	    if(n<2)
-	    System.out.println(+n+ " is not a prime number");
-	    
+	   
 	    for(int i=1;i<=n;i++)
 	    {
 	        if(n%i==0)
-	        count +=1;
+	        {
+	            count++;
+	        }
+	        
 	    }
-	    if (count>2)
-	     System.out.println(+n+ " is not a prime no");
-	     else 
+	    if (count==2)
 	     System.out.println(+n+" is a prime no.");
+	     else
+	     System.out.println(+n+ " is not a prime no");
 	}
 }
